c2e2ab358d1be4a9c67c1c10f4b5e4f7d1ab988f
ahmad1091 <ahmadtayeb91@gmail.com>
2020-09-28 12:10:12 +0300

    update all the files

--- java.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("motivation");
-    if (x.style.display === "none") 
-        x.style.display = "block";
-     else 
-        x.style.display = "none";
-        
-    
-}
