f210f3d559f80384fd61c905204005d9eac8ffb5
ahmad1091 <ahmadtayeb91@gmail.com>
2020-09-29 20:23:22 +0300

    modyfy the navbar

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 var main = document.getElementById("main");
+var nav = document.getElementById("nav");
+
+var sticky = nav.offsetTop;
+window.onscroll = function () {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
